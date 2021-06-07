94d91f69cbe7fbb14c47301a7ea44e685ddea70f
Brian Cappello <briancappello@gmail.com>
2021-06-07 13:42:39 -0400

    release v0.11.1

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -27,7 +27,7 @@ author = 'Brian Cappello'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = 'v0.11.0'
+release = 'v0.11.1'
 
 
 # -- General configuration ---------------------------------------------------
